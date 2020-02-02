6f980842a4778f8afebde894c9d8756f1cb61ce9
FtAlama <ajitsonam999@gmail.com>
2020-02-02 12:50:38 +0100

    first commit to main

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main
+{
+
+	public static void main(String[] agrs)
+	{
+		System.out.println("Coucou attendez je vais créé un pacagke je sais po encore comment faire mais tkt bibi sur le coups");
+	}
+}
